5277e0a576a26c72e0a6359a9a282ca2ec75a987
Raymond Zhu <rennechuh@gmail.com>
2016-04-17 18:21:16 +1000

    Add eslint

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,25 @@ var globby = require('globby');
 var through = require('through2');
 var gutil = require('gulp-util');
 var babelify = require('babelify');
+var eslint = require('gulp-eslint');
+var srcPaths = {
+  scripts: ['./src/**/*.jsx']
+};
+var destPaths = {
+  scripts: './dist/'
+}
 
-gulp.task('default', ['javascript'], function(){
+gulp.task('default', ['lint', 'transpile'], function(){
   console.log('gulp started');
 });
 
 
-gulp.task('javascript', function () {
+gulp.task('lint', function() {
+  gulp.src(srcPaths.scripts)
+      .pipe(eslint());
+});
+
+gulp.task('transpile', function () {
   // gulp expects tasks to return a stream, so we create one here.
   var bundledStream = through();
 
@@ -19,11 +31,11 @@ gulp.task('javascript', function () {
     // turns the output bundle stream into a stream containing
     // the normal attributes gulp plugins expect.
     .pipe(source('app.js'))
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest(destPaths.scripts));
 
   // "globby" replaces the normal "gulp.src" as Browserify
   // creates it's own readable stream.
-  globby(['./src/**/*.jsx']).then(function(entries) {
+  globby(srcPaths.scripts).then(function(entries) {
     // create the Browserify instance.
     var b = browserify({
       entries: entries
